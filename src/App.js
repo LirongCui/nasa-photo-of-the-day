86e7bd43c711675c61733ed6004e0ecc784d2fb4
Lirong <lirong@Lirongs-MacBook-Pro.local>
2021-03-17 21:32:42 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./App.css";
+import PhotoCard from './components/photocard'
 
 function App() {
+  const [nasadata, setNasaData] = useState([])
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      setNasaData(res.data)
+      console.log(res.data)
+    })
+    .catch( err => {
+      console.log(err);
+    })
+  },[])
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className='container'>
+      <h1>Welcome to Nasa Gallery</h1>
+        <div>
+          <PhotoCard data={nasadata}/>
+        </div>
     </div>
   );
 }
